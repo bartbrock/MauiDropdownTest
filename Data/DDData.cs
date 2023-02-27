b33269148f125cb79e2119d4da1dd9c5ea1bf628
Bart Brock <bbrock@clarkdataservices.com>
2023-02-27 16:57:37 -0500

    Updated with demo from Poulpynator

--- Data/DDData.cs
+++ Data/DDData.cs
@@ -14,25 +14,5 @@ namespace MauiDropdownTest.Data
         public string? Transfer_from_Code { get; set; }
         public string? Transfer_to_Code { get; set; }
 
-        public IList<DDData> Data { get; set; }
-    }
-
-    public class DData
-    { 
-       
-
-        //public DData()
-        //{
-        //    var dddata = new DDData();
-        //    //Data = new();
-        //    for (int i = 0; i < 9; i++)
-        //    {
-        //        dddata.No = $"{i}";
-        //        dddata.Transfer_from_Code = $"Transfer_From_{i}";
-        //        dddata.Transfer_to_Code = $"Transfer_To_{i}";
-        //        Data.Add(dddata);
-        //    }
-        //}
-
     }
 }
